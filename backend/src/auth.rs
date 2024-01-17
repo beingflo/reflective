af45d57c458ab8ba0eb28e3c479277e5e9faa638
Florian Marending <florian@marending.dev>
2024-01-17 21:07:26 +0100

    [be] implement auth flow

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -10,8 +10,30 @@ pub struct User {
     password: String,
 }
 
-pub async fn signup(Extension(state): Extension<State>, Json(user): Json<User>) -> StatusCode {
-    StatusCode::OK
+pub async fn signup(
+    Extension(state): Extension<State>,
+    Json(user): Json<User>,
+) -> Result<StatusCode, AppError> {
+    let connection = state.conn.lock().await;
+
+    let mut stmt = connection
+        .prepare("SELECT username, password FROM users WHERE username = ?1")
+        .unwrap();
+
+    let mut rows = stmt.query([&user.username]).unwrap();
+
+    // User already exists
+    if let Some(_) = rows.next()? {
+        return Ok(StatusCode::CONFLICT);
+    }
+
+    // No salting for now, randomly generated password anyway
+    connection.execute(
+        "INSERT INTO users (username, password) VALUES (?1, ?2)",
+        (&user.username, user.password),
+    )?;
+
+    Ok(StatusCode::OK)
 }
 
 pub async fn login(
@@ -25,7 +47,7 @@ pub async fn login(
         .unwrap();
     let mut rows = stmt.query([user.username]).unwrap();
 
-    let user = match rows.next().expect("DB failed") {
+    let db_user = match rows.next()? {
         Some(row) => User {
             username: row.get(0)?,
             password: row.get(1)?,
@@ -33,5 +55,9 @@ pub async fn login(
         None => return Ok(StatusCode::UNAUTHORIZED),
     };
 
-    Ok(StatusCode::OK)
+    if user.password == db_user.password {
+        Ok(StatusCode::OK)
+    } else {
+        Ok(StatusCode::UNAUTHORIZED)
+    }
 }
