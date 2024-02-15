3271b8006d2dd586c0d2c5353541007222f3d745
Florian Marending <florian@marending.dev>
2024-02-15 22:56:41 +0100

    [be] Add poc of presigned url generation

--- backend/src/image.rs
+++ backend/src/image.rs
@@ -1,5 +1,6 @@
 use crate::{auth::AuthenticatedUser, error::AppError};
 use axum::{extract::State, http::StatusCode, Json};
+use s3::{creds::Credentials, Bucket};
 use serde::{Deserialize, Serialize};
 
 use crate::AppState;
@@ -19,6 +20,17 @@ pub async fn upload_images(
     }
     // Create data.number * 3 presigned PUT URLs
     // Insert them into db with NULL metadata
+
+    let bucket_name = "reflective-test";
+    let region = s3::Region::Custom {
+        region: "XXX".to_string(),
+        endpoint: "XXX".to_string(),
+    };
+    let credentials = Credentials::new(Some("XXX"), Some("XXX"), None, None, None).unwrap();
+
+    let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
+
+    let url_put = bucket.presign_put("/test.file", 600, None).unwrap();
 
     Ok(StatusCode::OK)
 }
