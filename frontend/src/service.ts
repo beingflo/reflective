df59242a4659c4e89e4b684142c734351e4b7607
Florian Marending <florian@marending.dev>
2024-02-22 22:39:15 +0100

    [fe] Implement api calls

--- frontend/src/service.ts
+++ frontend/src/service.ts
@@ -1,6 +1,9 @@
+import { UpdateConfigRequest } from './types';
+
 export const login = async (username: string, password: string) => {
   const response = await fetch('/auth/login', {
     body: JSON.stringify({ username, password }),
+    method: 'POST',
   });
   // TODO handle response
 };
@@ -8,6 +11,28 @@ export const login = async (username: string, password: string) => {
 export const signup = async (username: string, password: string) => {
   const response = await fetch('/auth/signup', {
     body: JSON.stringify({ username, password }),
+    method: 'POST',
   });
+  // TODO handle response
+};
+
+export const updateConfig = async (request: UpdateConfigRequest) => {
+  const response = await fetch('/user/config', {
+    body: JSON.stringify(request),
+    method: 'PATCH',
+  });
+  // TODO handle response
+};
+
+export const getImageUploadLinks = async (number: number) => {
+  const response = await fetch('/images/upload', {
+    body: JSON.stringify({ number }),
+    method: 'POST',
+  });
+  // TODO handle response
+};
+
+export const getImageList = async () => {
+  const response = await fetch('/images');
   // TODO handle response
 };
