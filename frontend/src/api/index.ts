f27b4aef54fa83017b73571e1ef567053419e83a
Florian Marending <florian@marending.dev>
2024-02-25 21:33:21 +0100

    [fe] Start upload screen

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,21 +1,3 @@
-import { UpdateConfigRequest } from '../types';
-
-export const signup = async (username: string, password: string) => {
-  const response = await fetch('/auth/signup', {
-    body: JSON.stringify({ username, password }),
-    method: 'POST',
-  });
-  // TODO handle response
-};
-
-export const updateConfig = async (request: UpdateConfigRequest) => {
-  const response = await fetch('/user/config', {
-    body: JSON.stringify(request),
-    method: 'PATCH',
-  });
-  // TODO handle response
-};
-
 export const getImageUploadLinks = async (number: number) => {
   const response = await fetch('/images/upload', {
     body: JSON.stringify({ number }),
--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -1,7 +1,38 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component, onMount } from 'solid-js';
 
 const Upload: Component = () => {
-  return <p class="text-4xl text-black text-center py-20">Upload</p>;
+  const [content, setContent] = createSignal('');
+  let ref: HTMLInputElement;
+
+  onMount(() => {
+    ref.addEventListener('change', () => {
+      const reader = new FileReader();
+      reader.onload = (evt) => {
+        console.log(evt.target.result);
+      };
+      const numFiles = ref.files.length;
+      [...Array(numFiles).keys()].forEach((i: number) => {
+        reader.readAsText(ref.files.item(i));
+      });
+    });
+  });
+
+  return (
+    <div class="mx-auto flex flex-col w-1/2 min-w-96 pt-12">
+      <div class="flex flex-row gap-4 items-baseline">
+        <p class="text-4xl md:text-6xl mb-4 text-black dark:text-white font-extrabold">
+          Upload
+        </p>
+      </div>
+      <label
+        class="mt-8 text-center px-4 rounded-sm bg-white border border-black py-2 uppercase text-black hover:bg-gray-100 hover:shadow-none focus:outline-none hover:cursor-pointer"
+        for="file"
+      >
+        Select files
+      </label>
+      <input ref={ref} type="file" id="file" class="hidden" multiple />
+    </div>
+  );
 };
 
 export default Upload;
