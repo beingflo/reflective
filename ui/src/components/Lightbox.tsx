9b5a45f281282e858379f3cf9c022bb222eab952
Florian Marending <florian@marending.dev>
2025-01-13 21:43:00 +0100

    [ui] Open lightbox with fixed positioning above view instead of on a separate page

---
+++ ui/src/components/Lightbox.tsx
@@ -0,0 +1,49 @@
+import { useNavigate } from '@solidjs/router';
+import { createEffect, type Component } from 'solid-js';
+import { useStore } from '../store';
+
+export type LightboxProps = {
+  imageId: string;
+};
+
+const Lightbox: Component<LightboxProps> = (props: LightboxProps) => {
+  const navigate = useNavigate();
+  const [state, { setImages }] = useStore();
+
+  // Fetch images in case of reload
+  createEffect(async () => {
+    if (state.images.length === 0) {
+      const response = await fetch('/api/images', {
+        headers: {
+          'content-type': 'application/json',
+        },
+      }).then((response) => response.json());
+
+      setImages(response);
+    }
+  });
+
+  createEffect(() => {
+    const currentIndex = state.images.indexOf(props.imageId);
+    const nextImage = state.images[currentIndex + 1];
+    const lastImage = state.images[currentIndex - 1];
+
+    if (nextImage) {
+      new Image().src = `/api/images/${nextImage}?quality=medium`;
+    }
+    if (lastImage) {
+      new Image().src = `/api/images/${lastImage}?quality=medium`;
+    }
+  });
+
+  return (
+    <div class="fixed bg-white flex w-full h-screen p-2 md:p-8 justify-center">
+      <img
+        class="h-full w-full object-contain"
+        src={`/api/images/${props.imageId}?quality=medium`}
+      />
+    </div>
+  );
+};
+
+export default Lightbox;
--- ui/src/pages/View.tsx
+++ ui/src/pages/View.tsx
@@ -1,10 +1,61 @@
 import { useNavigate } from '@solidjs/router';
-import { type Component, createEffect, For } from 'solid-js';
+import {
+  type Component,
+  createEffect,
+  createSignal,
+  For,
+  onCleanup,
+  Show,
+} from 'solid-js';
 import { useStore } from '../store';
+import Lightbox from '../components/Lightbox';
+import { tinykeys } from 'tinykeys';
+
+export const validateEvent = (callback) => (event) => {
+  if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'TEXTAREA') {
+    event.preventDefault();
+    callback();
+  }
+};
 
 const View: Component = () => {
   const [state, { setImages }] = useStore();
-  const navigate = useNavigate();
+  const [openImage, setOpenImage] = createSignal('');
+
+  const goToNextImage = () => {
+    if (!openImage()) return;
+    const currentIndex = state.images.indexOf(openImage());
+    const nextImage = state.images[currentIndex + 1];
+
+    if (nextImage) {
+      setOpenImage(nextImage);
+    }
+  };
+
+  const goToLastImage = () => {
+    if (!openImage()) return;
+    const currentIndex = state.images.indexOf(openImage());
+    const lastImage = state.images[currentIndex - 1];
+
+    if (lastImage) {
+      setOpenImage(lastImage);
+    }
+  };
+
+  const closeLightbox = () => {
+    document
+      .getElementById(openImage())
+      ?.scrollIntoView({ behavior: 'smooth' });
+    setOpenImage('');
+  };
+
+  const cleanup = tinykeys(window, {
+    ArrowRight: validateEvent(goToNextImage),
+    ArrowLeft: validateEvent(goToLastImage),
+    Escape: closeLightbox,
+  });
+
+  onCleanup(cleanup);
 
   createEffect(async () => {
     const response = await fetch('/api/images', {
@@ -17,17 +68,21 @@ const View: Component = () => {
   });
 
   const openLightbox = (image: string) => {
-    navigate(`/${image}`);
+    setOpenImage(image);
   };
 
   return (
     <div>
+      <Show when={openImage()}>
+        <Lightbox imageId={openImage()} />
+      </Show>
       <div class="grid grid-cols-1 md:grid-cols-3 md:w-3/4 px-4 py-4 mx-auto gap-8">
         <For each={state.images}>
           {(image) => (
             <div class="aspect-square w-full">
               <img
                 class="object-cover w-full h-full"
+                id={image}
                 loading="lazy"
                 onClick={() => openLightbox(image)}
                 src={`/api/images/${image}?quality=small`}
