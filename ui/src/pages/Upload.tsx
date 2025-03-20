dd406c8033718fe4bfab50044ba507d492989441
Florian Marending <florian@marending.dev>
2025-03-20 21:56:59 +0100

    [ui] Increase upload parallelism

--- ui/src/pages/Upload.tsx
+++ ui/src/pages/Upload.tsx
@@ -73,7 +73,7 @@ const Upload: Component = () => {
         setImageStates((prev) => ({ ...prev, [image.name]: 'done' }));
       }
     },
-    { concurrency: 4 },
+    { concurrency: 8 },
   );
 
   createEffect(() => {
