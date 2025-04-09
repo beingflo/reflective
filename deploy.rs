2cc21190126636dc6a080a3aa2aacae56d69610b
Florian Marending <florian@marending.dev>
2025-04-09 23:06:05 +0200

    [infra] Start work on rust deploy script

--- deploy.rs
+++ deploy.rs
@@ -3,11 +3,14 @@
 ---
 [package]
 edition = "2021"
+version = "0.1.0"
 [dependencies]
 clap = { version = "4", features = ["derive"] }
+cmd_lib = { version = "1.9.5" }
 ---
 
 use clap::Parser;
+use cmd_lib::*;
 
 #[derive(Parser, Debug)]
 #[clap(version)]
@@ -16,7 +19,19 @@ struct Args {
     config: Option<std::path::PathBuf>,
 }
 
-fn main() {
-    let args = Args::parse();
-    println!("{:?}", args);
+#[cmd_lib::main]
+fn main() -> CmdResult {
+    let _args = Args::parse();
+
+    run_cmd! (
+        warn "You're deploying reflective to production"
+    )?;
+
+    let status = run_fun! (
+        git status --porcelain
+    )?;
+
+    println!("{}", status.len());
+
+    Ok(())
 }
