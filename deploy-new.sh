a7deadafc98c8544595264ae2a79d16751b3a926
Florian Marending <florian@marending.dev>
2025-04-27 20:47:21 +0200

    [infra] Improve deploy script

--- deploy-new.sh
+++ deploy-new.sh
@@ -5,7 +5,7 @@ echo -e "Deploying reflective to production!"
 
 [ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
 
-( cd service ; version=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' | tr -d '"') )
+cd service; version=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' | tr -d '"'); cd ..
 
 echo "Latest version: ${version}"
 
@@ -15,7 +15,7 @@ read -p "Enter version to be deployed [${next_version}]: " new_version
 
 new_version=${new_version:-${next_version}}
 
-( cd service ; cargo set-version "${new_version}" || die "Failed to set version in Cargo.toml" )
+cd service; cargo set-version "${new_version}" || die "Failed to set version in Cargo.toml"; cd ..
 
 docker buildx build --platform=linux/amd64 -t "ghcr.io/beingflo/reflective:${new_version}" . || die "Failed to build docker image"
 docker push "ghcr.io/beingflo/reflective:${new_version}" || die "Failed to push docker image"
