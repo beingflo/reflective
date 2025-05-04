0330b12da13f972c85691b3f5666ad447d02580e
Florian Marending <florian@marending.dev>
2025-05-04 16:14:05 +0200

    [infra] refactor install script

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 die() { echo "$*" 1>&2 ; exit 1; }
 
-./build || die "Failed to build docker image"
-./install || die "Failed to deploy to server"
+./build.sh || die "Failed to build docker image"
+./deploy.sh || die "Failed to deploy to server"
